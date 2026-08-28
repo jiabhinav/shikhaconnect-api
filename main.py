09aa0be1b23dff4f8b93ba8b1ede9c5506b1c923
Abhinav <abhinavshuklaji1992@gmail.com>
2026-08-29 04:17:46 +0530

    fixed orgin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.database import Base, engine, sync_missing_columns, test_db_connection
 from routers.auth import router as auth_router
@@ -11,6 +12,14 @@ sync_missing_columns()
 app = FastAPI(
     title="School Management API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, you can restrict this to specific domains if needed (e.g., ["http://127.0.0.1:5173", "https://yourfrontend.com"])
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(auth_router)
