e83e3151ffae40fc4775b715472c5ff62544ed34
Abhinav <abhinavshuklaji1992@gmail.com>
2026-09-11 21:05:49 +0530

    fix cross policy

--- main.py
+++ main.py
@@ -32,12 +32,14 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://127.0.0.1:4173",
-        "https://localhost:4173",
-        "https://127.0.0.1:5173",
-        "https://localhost:5173",
+        "http://127.0.0.1:4173",
+        "http://localhost:4173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
         "https://shikshaconnect.com",
         "https://www.shikshaconnect.com",
+        "https://dev.shikshaconnect.com",
+        "https://www.school.shikshaconnect.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
